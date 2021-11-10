855790e8d809bf1992c245b4bb3588fa29064eb0
Namelessafk <marcusthuliok9@gmail.com>
2021-11-10 00:44:06 -0300

    Limpando o app.component.html e gerando o componente pra o app

---
+++ src/app/contatos-listagem/contatos-listagem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contatos-listagem',
+  templateUrl: './contatos-listagem.component.html',
+  styleUrls: ['./contatos-listagem.component.css']
+})
+export class ContatosListagemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
